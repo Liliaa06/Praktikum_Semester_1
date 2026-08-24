7832cbe88fbe89a760caa7f0c24439ffecf8374f
Liliaa06 <xyliaaputri06@gmail.com>
2026-08-24 22:48:15 +0700

    Add files via upload

---
+++ pertemuan_1.py
@@ -0,0 +1,7 @@
+print("Amallia Putri Nurul Aziz");
+#ini contoh komentar multi baris-1
+#ini contoh komentar multi baris-2
+print("\n2605060058"); #kegunaan \n untuk membuat baris baru
+print("\tCilacap, 06 Januari 2007"); #kegunaan \t untuk membuat tab
+print("Perempuan"); 
+print("Menyanyi da\bn Membaca"); #kegunaan \b untuk membuat backspace maka huruf sebelumnya akan hilang
